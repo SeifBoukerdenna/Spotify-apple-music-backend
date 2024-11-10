0b672d30ea022ffed726c3361e3c4eda948bc326
Seif Boukerdenna <elmelz6472@gmail.com>
2024-11-10 11:28:20 -0500

    added dotenv and fixed config

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,7 @@ console.log(`Running in ${isRenderEnv ? "Render" : "Local"} environment`);
 const config = {
   privateKeyPath: isRenderEnv
     ? "/etc/secrets/AuthKey_8MQTX7S357.p8"
-    : path.resolve(__dirname, "../etc/secrets/AuthKey_8MQTX7S357.p8"),
-
-  // Other configuration options
+    : path.resolve(__dirname, "../etc/secrets/AuthKey_8MQTX7S357.p8"), // Local path
   teamId: process.env.TEAM_ID || "VZR89A8Z89",
   keyId: process.env.KEY_ID || "8MQTX7S357",
   tokenExpiresIn: "180d",
