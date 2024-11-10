541de7c15d1d3534d1b4e74bf90cad0d7dc80d20
Seif Boukerdenna <elmelz6472@gmail.com>
2024-11-10 11:23:34 -0500

    added dotenv and fixed config

--- dist/config.js
+++ dist/config.js
@@ -4,14 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 const isRenderEnv = process.env.RENDER_ENV === "true";
 console.log(`Running in ${isRenderEnv ? "Render" : "Local"} environment`);
 const config = {
     privateKeyPath: isRenderEnv
-        ? "/etc/secrets/AuthKey_8MQTX7S357.p8" // Absolute path for Render
-        : path_1.default.resolve(__dirname, "../etc/secrets/AuthKey_8MQTX7S357.p8"), // Local path in root
+        ? "/etc/secrets/AuthKey_8MQTX7S357.p8"
+        : path_1.default.resolve(__dirname, "../etc/secrets/AuthKey_8MQTX7S357.p8"),
+    // Other configuration options
     teamId: process.env.TEAM_ID || "VZR89A8Z89",
     keyId: process.env.KEY_ID || "8MQTX7S357",
     tokenExpiresIn: "180d",
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,4 @@
 import path from "path";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const isRenderEnv = process.env.RENDER_ENV === "true";
 
@@ -9,8 +6,10 @@ console.log(`Running in ${isRenderEnv ? "Render" : "Local"} environment`);
 
 const config = {
   privateKeyPath: isRenderEnv
-    ? "/etc/secrets/AuthKey_8MQTX7S357.p8" // Absolute path for Render
-    : path.resolve(__dirname, "../etc/secrets/AuthKey_8MQTX7S357.p8"), // Local path in root
+    ? "/etc/secrets/AuthKey_8MQTX7S357.p8"
+    : path.resolve(__dirname, "../etc/secrets/AuthKey_8MQTX7S357.p8"),
+
+  // Other configuration options
   teamId: process.env.TEAM_ID || "VZR89A8Z89",
   keyId: process.env.KEY_ID || "8MQTX7S357",
   tokenExpiresIn: "180d",
