23a1b10e8cc836ad5f89b8e034bf0ffd9135bf8f
Seif Boukerdenna <elmelz6472@gmail.com>
2024-11-11 13:53:43 -0500

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,48 @@
 import express from "express";
+import cors from "cors"; // Import the cors package
 import tokenRoutes from "@/routes/tokenRoutes";
 
 // Initialize Express app
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Define allowed origins
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://spotify-to-apple-music-three.vercel.app",
+];
+
+// CORS configuration options
+interface CorsOptions {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => void;
+  methods: string;
+  credentials: boolean;
+}
+
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      // If the origin is in the allowed list, allow the request
+      callback(null, true);
+    } else {
+      // If the origin is not allowed, reject the request
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
+  credentials: true, // Allow cookies and other credentials
+};
+
+// Apply the CORS middleware to all incoming requests
+app.use(cors(corsOptions));
+
+// If you want to enable CORS for specific routes only, you can apply it like this:
+// app.use("/api", cors(corsOptions), tokenRoutes);
 
 // Route setup
 app.use("/api", tokenRoutes);
